3404f293a35cea61c6c2d9492b0eb14044d6e2fe
alien-house <info@alien-house.com>
2017-07-05 13:09:10 -0700

    first app

---
+++ MyApplication/app/src/main/java/com/example/shinji/myapplication/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.shinji.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+/**
+ * Created by shinji on 2017/07/04.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+
+}
