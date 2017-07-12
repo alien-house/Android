9d187994563c287c8088a68d87151d132a2c4f9f
alien-house <info@alien-house.com>
2017-07-12 10:24:20 -0700

    add

--- View/app/src/main/java/com/example/shinji/view/SimpleMenuActivity.java
+++ View/app/src/main/java/com/example/shinji/view/SimpleMenuActivity.java
@@ -2,14 +2,18 @@ package com.example.shinji.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by shinji on 2017/07/11.
  */
 
 public class SimpleMenuActivity extends AppCompatActivity {
+    ListView l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +23,20 @@ public class SimpleMenuActivity extends AppCompatActivity {
         System.out.println("================");
         System.out.println(listText);
         ArrayAdapter a = new ArrayAdapter<String>(this,R.layout.list_data,listText);
-        ListView l = (ListView)findViewById(R.id.ListView_menu);
+        l = (ListView)findViewById(R.id.ListView_menu);
         l.setAdapter(a);
+
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                int itemPosition = position;
+                String itemValue = (String)l.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+ itemPosition + " ListItem : "+ itemValue,
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+
     }
 }
